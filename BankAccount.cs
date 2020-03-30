09020eeb9cdce68fe9982bd1d316ada4e8d57912
SorenHersomNielsen <hersom@ellekrattet.dk>
2020-03-30 13:45:05 +0200

    Initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,42 @@
+﻿namespace BankV05
+{
+    /// <summary>
+    /// This class represents a very simple bank account.
+    /// Only the amount of money on the account is represented.
+    /// </summary>
+    public class BankAccount
+    {
+        private double _balance;
+        private string _accountHolderName;
+
+        public BankAccount(string accountHolderName)
+        {
+            _balance = 0.0;
+            _accountHolderName = accountHolderName;
+        }
+
+        public double Balance
+        {
+            get { return _balance;}
+            
+        }
+      
+          public string accountHolderName
+        {
+          get {return _accountHolderName;}
+        }
+
+        public void Deposit(double amount)
+        {
+            _balance = _balance + amount;
+        }
+/// <summary>
+/// Withdraws some money 
+/// </summary>
+/// <param name="amount"></param>
+        public void Withdraw(double amount)
+        {
+            _balance = _balance - amount;
+        }
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BankV05
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            BankAccount myAccount = new BankAccount("Søren");
+            BankAccount myAccount2 = new BankAccount("Chris");
+            BankAccount myAccount3 = new BankAccount("Line");
+
+            myAccount.Deposit(2000);
+            Console.WriteLine($"Account holder {myAccount.accountHolderName} and balance is : {myAccount.Balance}");
+           
+            myAccount.Withdraw(1500);
+            Console.WriteLine($"Account holder {myAccount.accountHolderName} and balance is : {myAccount.Balance}");
+
+            myAccount2.Deposit(5000);
+            Console.WriteLine($"Account holder {myAccount2.accountHolderName} and balance is : {myAccount2.Balance}");
+           
+            myAccount2.Withdraw(1500);
+            Console.WriteLine($"Account holder {myAccount2.accountHolderName} and balance is : {myAccount2.Balance}");
+
+            myAccount3.Deposit(10000);
+            Console.WriteLine($"Account holder {myAccount3.accountHolderName} and balance is : {myAccount3.Balance}");
+           
+            myAccount3.Withdraw(3000);
+            Console.WriteLine($"Account holder {myAccount3.accountHolderName} and balance is : {myAccount3.Balance}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
